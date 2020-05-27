eb990540eeb80cd6c9ca447a07e455f13b6afa51
yangke <yangke1@liepin.com>
2020-05-27 13:57:21 +0800

    33

--- cmd.js
+++ cmd.js
@@ -2,58 +2,34 @@ var inquirer = require('inquirer');
 const { execSync } = require('child_process');
 var { logger } = require('./logger');
 (() => {
- 
+  // let data = execSync(`git tag --sort=-v:refname`)
+  // .toString()
+  // .split('\n')
+  // .filter(_=> _!== '')
   inquirer.prompt([
     {
       name: 'version',
       type: 'list',
       message: '版本号',
-      choices: ['major', 'minor', 'patch', 'CustomVersion'],
-    },
-    {
-      name: 'customVersion',
-      message: '请输入版本号',
-      when: ({ version }) => version === 'CustomVersion',
+      choices: ['major', 'minor', 'patch'],
     },
     {
       name: 'description',
       message: '请输入描述信息',
       type: 'input',
     },
-  ]).then(({version, CustomVersion, description})=> {
-
-    isClean()
-    .then(clean => {
-      console.log(clean)
-      // try {
-      //   console.log(version, CustomVersion, description, execSync)
-      //   logger.info('执行提交', version, 'end')
-      //   // execSync(`npm version ${version}`)
-        tag(version === 'CustomVersion' ? CustomVersion : version, description, clean);
-      // } catch(e) {
-      //   logger.error(e)
-      // }
-    })
-    // execSync(`npm version ${version}`)  {version, CustomVersion, description}
-  
+  ]).then(({version, description})=> {
+    execSync(`git commit -a -m '${description}' `)
+    let data =  execSync(`npm version ${version}`)
+    execSync(`git push origin ${data}`)
+    execSync('git push')
   })
 })();
 
 function tag(version = 'patch', description, isClean) {
-  console.log('clean', isClean)
   return new Promise((res, rej) => {
     let data
     try {
-      
-      // execSync('git add .')
-      // execSync(`git commit -m '${description}' `)
-      // data = execSync(`npm version ${version}`)
-      // logger.info(`版本号为: ${data}`)
-      // execSync(`git push origin ${data}`)
-      // execSync('git push')
-
-    
-
       if(isClean) {
         data = execSync(`npm version ${version}`)
         logger.info(`版本号为: ${data}`)
@@ -75,7 +51,6 @@ function tag(version = 'patch', description, isClean) {
 function isClean() {
   return new Promise((resolve, reject) => {
     let status = execSync('git status').toString();
-    // console.log('检查代码更新', status)
     if (status.indexOf('working tree clean') > -1) {
       console.log('\x1B[32m%s\x1b[0m', 'nothing to commit, working tree clean');
       resolve(true);
--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 let version = '3.0.0';
+
+/*
+* -1 不需要更新
+* 0 自动更新
+* 1 手动更新
+*/ 
+function compare(current, latest) {
+  if(current === latest) return -1
+  let a = current.split('.').map(item=> item.replace('v',''));
+  let b = latest.split('.').map(item=> item.replace('v',''));
+  let len = a.length - b.length;
+  let type = 1;
+  if (len > 0) {
+      type = 2;
+  } else if (len < 0) {
+      type = 1;
+  }
+  len = Math.abs(len);
+  for (let i = 0; i < len; i++) {
+      type == 1 ? a.push('0') : b.push('0');
+  }
+  for (let k = 0; k < a.length; k++) {
+    let d = a[k].charCodeAt(0), 
+      c = b[k].charCodeAt(0)
+      if (d == c) {
+          continue
+      }
+      return k=== a.length - 1 ?  0 : 1;
+  }
+  return -1
+}
